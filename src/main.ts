5b08e889e2d52fa70eea3327d0b81b97e768aa56
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-09-30 14:04:08 +0200

    Documentation de l'API

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  const config = new DocumentBuilder()
+    .setTitle('TKORP Test API')
+    .setDescription("Documentation de l'API du test technique")
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
   app.enableCors({
     origin: ['http://localhost:3000', 'http://localhost:3001'], // Remplace par l'URL de ton front-end
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
